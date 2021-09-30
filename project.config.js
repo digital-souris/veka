5d87e0ce448b2441c5f2593a5d389cba5bb65bd4
digital-souris <85878712+digital-souris@users.noreply.github.com>
2021-09-30 13:40:32 +0300

    build

--- project.config.js
+++ project.config.js
@@ -8,5 +8,5 @@ module.exports = {
   minifyJS: true,
   minifyCSS: true,
   homePage: "index.html",
-  templateFiles: ["./src/pages/index.jade"]
+  templateFiles: ["./src/pages/index.jade", "./src/pages/catalog.jade"]
 };
