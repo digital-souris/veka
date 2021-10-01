537dced0cbfaf43b9e99526735c09b48daa723b7
digital-souris <85878712+digital-souris@users.noreply.github.com>
2021-10-01 13:09:03 +0300

    123

--- project.config.js
+++ project.config.js
@@ -1,17 +1,18 @@
 // Override Defaults
 
 module.exports = {
-  srcPath: "./src",
-  templatePath: "./src/pages",
-  buildPath: "./dist",
-  minifyHTML: true,
-  minifyJS: true,
-  minifyCSS: true,
-  homePage: "index.html",
-  templateFiles: [
-      "./src/pages/index.jade",
-    "./src/pages/catalog.jade",
-    './src/pages/catalog-list.jade',
-    './src/pages/news.jade'
-  ]
+    srcPath: "./src",
+    templatePath: "./src/pages",
+    buildPath: "./dist",
+    minifyHTML: true,
+    minifyJS: true,
+    minifyCSS: true,
+    homePage: "index.html",
+    templateFiles: [
+        "./src/pages/index.jade",
+        "./src/pages/catalog.jade",
+        './src/pages/catalog-list.jade',
+        './src/pages/news.jade',
+        './src/pages/news-item.jade'
+    ]
 };
