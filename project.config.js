fc1d0ea7b22eb42d1b45f2b62adf2fad914bb400
digital-souris <85878712+digital-souris@users.noreply.github.com>
2021-10-04 14:32:34 +0300

    contacts

--- project.config.js
+++ project.config.js
@@ -15,6 +15,7 @@ module.exports = {
         './src/pages/news.jade',
         './src/pages/news-item.jade',
         './src/pages/return.jade',
-        './src/pages/delivery.jade'
+        './src/pages/delivery.jade',
+        './src/pages/contacts.jade'
     ]
 };
